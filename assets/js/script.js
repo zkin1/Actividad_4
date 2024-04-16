011e475d430f67bbde0fc1487a869de6656af950
zkin1 <87878010+zkin1@users.noreply.github.com>
2024-04-15 20:45:33 -0400

    barra de busqueda en index agregada

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+// Array de productos
+const productos = [
+    {
+      nombre: "Ojos",
+      imagen: "./assets/img/ojos.png",
+      descripcion: "Una descripción detallada de los ojos. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur urna quis eros sollicitudin, sed hendrerit justo bibendum."
+    },
+    {
+      nombre: "Mineon",
+      imagen: "./assets/img/mineon.png",
+      descripcion: "Una descripción detallada de Mineon. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur urna quis eros sollicitudin, sed hendrerit justo bibendum."
+    },
+    {
+      nombre: "AUTO",
+      imagen: "./assets/img/auto.png",
+      descripcion: "Una descripción detallada de AUTO. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur urna quis eros sollicitudin, sed hendrerit justo bibendum."
+    }
+  ];
+  
+  // Función para realizar la búsqueda
+  function search() {
+    const searchText = document.getElementById("searchInput").value.toLowerCase();
+    const resultados = productos.filter(producto =>
+      producto.nombre.toLowerCase().includes(searchText)
+    );
+  
+    mostrarResultados(resultados);
+  }
+  
+  // Función para mostrar los resultados de la búsqueda
+  function mostrarResultados(resultados) {
+    const contenedorResultados = document.getElementById("resultadosBusqueda");
+    contenedorResultados.innerHTML = "";
+  
+    if (resultados.length === 0) {
+      contenedorResultados.innerHTML = "<p>No se encontraron resultados.</p>";
+      return;
+    }
+  
+    resultados.forEach(producto => {
+      const divProducto = document.createElement("div");
+      divProducto.classList.add("card", "mb-3");
+      divProducto.innerHTML = `
+        <div class="row no-gutters">
+          <div class="col-md-4">
+            <img src="${producto.imagen}" class="card-img" alt="${producto.nombre}">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${producto.nombre}</h5>
+              <p class="card-text">${producto.descripcion}</p>
+            </div>
+          </div>
+        </div>
+      `;
+      contenedorResultados.appendChild(divProducto);
+    });
+  }
